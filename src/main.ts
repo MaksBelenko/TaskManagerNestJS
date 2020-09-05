66296ce33ed0eddb6907494063df91e8a087724c
MaksBelenko <maksim.belenko@gmail.com>
2020-09-05 13:41:27 +0100

    added environmental variables

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as config from 'config';
 
 async function bootstrap() {
-  const logger = new Logger('bootstrap');
+    const serverConfig = config.get('server');
+    const logger = new Logger('bootstrap');
+    const app = await NestFactory.create(AppModule);
 
-  const app = await NestFactory.create(AppModule);
-  
-  const port = 3000;
-  await app.listen(port);
-  logger.log(`Application started listening on port ${port}`)
+    if (process.env.NODE_ENV === 'development') {
+        app.enableCors();
+    }
+
+    const port = process.env.PORT || serverConfig.port;
+    await app.listen(port);
+    logger.log(`Application started listening on port ${port}`);
 }
 bootstrap();
